0da9ab3255da104ff45871aebcbda8c6e2ee6bc5
Morton Fox <github@qslw.com>
2020-10-06 22:14:23 -0400

    Accept file and date arguments. Actually post the CSV data

--- import.rb
+++ import.rb
@@ -7,6 +7,15 @@ require 'date'
 require 'ferrum'
 require 'netrc'
 
+unless ARGV.size >= 3
+  warn "Usage: #{$PROGRAM_NAME} csv-file start-date end-date"
+  exit 1
+end
+
+csvfname = ARGV.shift
+startdate = Date.parse(ARGV.shift)
+enddate = Date.parse(ARGV.shift)
+
 netrc = Netrc.read
 entry = netrc['dailydiary.com']
 unless entry
@@ -16,16 +25,16 @@ end
 
 user, pass = entry
 
-csvdata = CSV.open('Libra_2020-10-06.csv', 'r', col_sep: ';') { |csv|
+csvdata = CSV.open(csvfname, 'r', col_sep: ';') { |csv|
   csv.map { |row|
     next if row.size < 2 || row[0].start_with?('#')
-    [Date.parse(row[0]), row[1].to_f.round(1)]
+
+    date = Date.parse(row[0])
+    next if date < startdate || date > enddate
+
+    [date, row[1].to_f.round(1)]
   }.compact
 }
-
-p csvdata
-
-exit 0
 
 browser = Ferrum::Browser.new
 browser.goto('https://www.dailydiary.com/signin')
@@ -33,13 +42,20 @@ browser.at_css('#Email').focus.type(user)
 browser.at_css('#Password').focus.type(pass)
 browser.at_css('#submit_form').click
 
-browser.goto('https://www.dailydiary.com/myquestions/892755/how-much-do-you-weigh-today/answer')
-browser.at_css('#AnswerDate').focus.type('10012020')
-browser.at_css('#AnswerTime').focus.type('1000A')
-browser.at_css('#ValueA').evaluate('this.value = "202.6"')
-browser.at_css('#submit_save').click
+csvdata.each { |date, value|
+  puts "Posting data for #{date} ..."
 
-browser.screenshot(path: 'hello.png')
+  browser.goto('https://www.dailydiary.com/myquestions/892755/how-much-do-you-weigh-today/answer')
+  browser.at_css('#AnswerDate').focus.type(date.strftime('%m%d%Y'))
+  browser.at_css('#AnswerTime').focus.type('1000A')
+  browser.at_css('#ValueA').evaluate("this.value = '#{value}'")
+
+  # browser.screenshot(path: 'hello.png')
+
+  browser.at_css('#submit_save').click
+
+  sleep(1)
+}
 
 browser.quit
 
